d3304408de4ef85463ab7b92bac7392896cfbd81
Kevin Wu <kevin.wu@live.com.my>
2017-12-01 13:44:59 +0800

    very simple form flow 3

--- Chatbot101/Chatbot101/Forms/LeaveForm.cs
+++ Chatbot101/Chatbot101/Forms/LeaveForm.cs
@@ -11,24 +11,35 @@ namespace Chatbot101.Forms
         "Please try again.",
         "My apologies, I didn’t understand '{0}'.",
         "Excuse me, I didn’t quite get that.",
-        "Sorry, but I’m a chatbot and don’t know what '{ 0}' means.")]
+        "Sorry, but I’m a chatbot and don’t know what '{0}' means.")]
     public class LeaveForm
     {
         [Describe(Description = "Your name")]
         [Prompt("Please tell me your name (current value: {})")]
         public string Name { get; set; }
+
         [Describe(Description = "Your email")]
         [Prompt("Please tell me your email (current value: {})")]
+        [Pattern(@".+@.+\..+")]
         public string Email { get; set; }
+
         [Describe(Description = "The type of leave you want to apply")]
         [Prompt("What type of leave do you want to apply? (current value: {})  {||}")]
         public LeaveType LeaveType { get; set; }
+
         [Describe(Description = "The leave date starting from")]
         [Prompt("What date will your leave start? (current value: {})")]
         public DateTime LeaveFrom { get; set; }
+
         [Describe(Description = "The leave ending date")]
         [Prompt("What date will your leave end? (current value: {})")]
         public DateTime LeaveTo { get; set; }
+
+        [Optional]
+        [Numeric(1, 5)]
+        [Describe(Description = "Rate the leave application service")]
+        [Prompt("Please rate how much you like our leave application service\n\n (Choose 1 to 5) (current value: {})")]
+        public int? Rating { get; set; }  // when select Optional, the value type must be nullable
 
         public IForm<LeaveForm> BuildForm()
         {
